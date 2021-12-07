882593a32096aeac55a7253d006911df48a4fec2
Martin Buezas <martinbuezas@gmail.com>
2021-12-07 07:04:46 -0300

    fix(share-file): Better error messages

--- packages/nativescript-share-file/index.android.ts
+++ packages/nativescript-share-file/index.android.ts
@@ -2,130 +2,132 @@ import { Application, File } from '@nativescript/core';
 import { ShareFileCommon } from './common';
 
 export class ShareFile extends ShareFileCommon {
-  open(args: any): boolean {
-    if (!args.path) {
-      console.log('ShareFile: Please add a file path');
-      return false;
-    }
+	open(args: any): boolean {
+		if (!args.path) {
+			console.log('ShareFile: Please add a file path');
+			return false;
+		}
 
-    try {
-      let intent = new android.content.Intent();
-      let map = android.webkit.MimeTypeMap.getSingleton();
-      let mimeType = map.getMimeTypeFromExtension(this.fileExtension(args.path));
-      mimeType = mimeType ? mimeType : "message/rfc822";
+		try {
+			let intent = new android.content.Intent();
+			let map = android.webkit.MimeTypeMap.getSingleton();
+			let mimeType = map.getMimeTypeFromExtension(this.fileExtension(args.path));
+			mimeType = mimeType ? mimeType : 'message/rfc822';
 
-      intent.addFlags(android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION);
+			intent.addFlags(android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
-      let uris = new java.util.ArrayList();
-      let uri = this.getUriForPath(args.path, '/' + this.fileName(args.path), Application.android.context);
-      uris.add(uri);
-      let builder = new android.os.StrictMode.VmPolicy.Builder();
-      android.os.StrictMode.setVmPolicy(builder.build());
+			let uris = new java.util.ArrayList();
+			let uri = this.getUriForPath(args.path, '/' + this.fileName(args.path), Application.android.context);
+			uris.add(uri);
+			let builder = new android.os.StrictMode.VmPolicy.Builder();
+			android.os.StrictMode.setVmPolicy(builder.build());
 
-      intent.setAction(android.content.Intent.ACTION_SEND_MULTIPLE);
-      intent.setType(mimeType);
-      intent.putParcelableArrayListExtra(android.content.Intent.EXTRA_STREAM, uris);
+			intent.setAction(android.content.Intent.ACTION_SEND_MULTIPLE);
+			intent.setType(mimeType);
+			intent.putParcelableArrayListExtra(android.content.Intent.EXTRA_STREAM, uris);
 
-      Application.android.foregroundActivity.startActivity(
-        android.content.Intent.createChooser(intent, args.intentTitle ? args.intentTitle : 'Open file:')
-      );
+			Application.android.foregroundActivity.startActivity(android.content.Intent.createChooser(intent, args.intentTitle ? args.intentTitle : 'Open file:'));
 
-      return true;
-    } catch (e) {
-      console.log('ShareFile: Android intent failed');
-      return false;
-    }
-  }
+			return true;
+		} catch (e) {
+			console.log('ShareFile: Android intent failed', e);
+			return false;
+		}
+	}
 
-  private fileExtension(filename) {
-    return filename.split('.').pop();
-  }
+	private fileExtension(filename) {
+		return filename.split('.').pop();
+	}
 
-  private fileName(filename) {
-    return filename.split('/').pop();
-  }
+	private fileName(filename) {
+		return filename.split('/').pop();
+	}
 
-  private getUriForPath(path, fileName, ctx) {
-    if (path.indexOf("file:///") === 0) {
-      return this.getUriForAbsolutePath(path);
-    } else if (path.indexOf("file://") === 0) {
-      return this.getUriForAssetPath(path, fileName, ctx);
-    } else if (path.indexOf("base64:") === 0) {
-      return this.getUriForBase64Content(path, fileName, ctx);
-    } else {
-      if (path.indexOf(ctx.getPackageName()) > -1) {
-        return this.getUriForAssetPath(path, fileName, ctx);
-      } else {
-        return this.getUriForAbsolutePath(path);
-      }
-    }
-  }
+	private getUriForPath(path, fileName, ctx) {
+		if (path.indexOf('file:///') === 0) {
+			return this.getUriForAbsolutePath(path);
+		} else if (path.indexOf('file://') === 0) {
+			return this.getUriForAssetPath(path, fileName, ctx);
+		} else if (path.indexOf('base64:') === 0) {
+			return this.getUriForBase64Content(path, fileName, ctx);
+		} else {
+			if (path.indexOf(ctx.getPackageName()) > -1) {
+				return this.getUriForAssetPath(path, fileName, ctx);
+			} else {
+				return this.getUriForAbsolutePath(path);
+			}
+		}
+	}
 
-  private getUriForAbsolutePath(path) {
-    let absPath = path.replace("file://", "");
-    let file = new java.io.File(absPath);
+	private getUriForAbsolutePath(path) {
+		let absPath = path.replace('file://', '');
+		let file = new java.io.File(absPath);
 
-    if (!file.exists()) {
-      console.log("File not found: " + file.getAbsolutePath());
-      return null;
-    } else {
-      return android.net.Uri.fromFile(file);
-    }
-  }
+		if (!file.exists()) {
+			console.log('File not found: ' + file.getAbsolutePath());
+			return null;
+		} else {
+			return android.net.Uri.fromFile(file);
+		}
+	}
 
-  private getUriForAssetPath(path, fileName, ctx) {
-    path = path.replace("file://", "/");
+	private getUriForAssetPath(path, fileName, ctx) {
+		path = path.replace('file://', '/');
 
-    if (!File.exists(path)) {
-      console.log("File does not exist: " + path);
-      return null;
-    }
+		if (!File.exists(path)) {
+			console.log('File does not exist: ' + path);
+			return null;
+		}
 
-    let localFile = File.fromPath(path);
-    let localFileContents = localFile.readSync(function (e) { console.log(e); });
-    let cacheFileName = this.writeBytesToFile(ctx, fileName, localFileContents);
+		let localFile = File.fromPath(path);
+		let localFileContents = localFile.readSync(function (e) {
+			console.log(e);
+		});
+		let cacheFileName = this.writeBytesToFile(ctx, fileName, localFileContents);
 
-    if (cacheFileName.indexOf("file://") === -1) {
-      cacheFileName = "file://" + cacheFileName;
-    }
+		if (cacheFileName.indexOf('file://') === -1) {
+			cacheFileName = 'file://' + cacheFileName;
+		}
 
-    return android.net.Uri.parse(cacheFileName);
-  }
+		return android.net.Uri.parse(cacheFileName);
+	}
 
-  private getUriForBase64Content(path, fileName, ctx) {
-    let resData = path.substring(path.indexOf("://") + 3);
-    let bytes;
+	private getUriForBase64Content(path, fileName, ctx) {
+		let resData = path.substring(path.indexOf('://') + 3);
+		let bytes;
 
-    try {
-      bytes = android.util.Base64.decode(resData, 0);
-    } catch (ex) {
-      console.log("Invalid Base64 string: " + resData);
-      return android.net.Uri.EMPTY;
-    }
+		try {
+			bytes = android.util.Base64.decode(resData, 0);
+		} catch (ex) {
+			console.log('Invalid Base64 string: ' + resData);
+			return android.net.Uri.EMPTY;
+		}
 
-    let cacheFileName = this.writeBytesToFile(ctx, fileName, bytes);
+		let cacheFileName = this.writeBytesToFile(ctx, fileName, bytes);
 
-    return android.net.Uri.parse(cacheFileName);
-  }
+		return android.net.Uri.parse(cacheFileName);
+	}
 
-  private writeBytesToFile(ctx, fileName, contents) {
-    let dir = ctx.getExternalCacheDir();
+	private writeBytesToFile(ctx, fileName, contents) {
+		let dir = ctx.getExternalCacheDir();
 
-    if (dir === null) {
-      console.log("Missing external cache dir");
-      return null;
-    }
+		if (dir === null) {
+			console.log('Missing external cache dir');
+			return null;
+		}
 
-    let storage = dir.toString() + "/filecomposer";
-    let cacheFileName = storage + "/" + fileName;
-    let toFile = File.fromPath(cacheFileName);
+		let storage = dir.toString() + '/filecomposer';
+		let cacheFileName = storage + '/' + fileName;
+		let toFile = File.fromPath(cacheFileName);
 
-    toFile.writeSync(contents, function (e) { console.log(e); });
+		toFile.writeSync(contents, function (e) {
+			console.log(e);
+		});
 
-    if (cacheFileName.indexOf("file://") === -1) {
-      cacheFileName = "file://" + cacheFileName;
-    }
+		if (cacheFileName.indexOf('file://') === -1) {
+			cacheFileName = 'file://' + cacheFileName;
+		}
 
-    return cacheFileName;
-  }
+		return cacheFileName;
+	}
 }
--- packages/nativescript-share-file/index.ios.ts
+++ packages/nativescript-share-file/index.ios.ts
@@ -1,85 +1,74 @@
 import { ShareFileCommon } from './common';
 
 export class ShareFile extends ShareFileCommon {
+	static getter<T>(_this2: any, property: T | { (): T }): T {
+		if (typeof property === 'function') {
+			return (<{ (): T }>property).call(_this2);
+		} else {
+			return <T>property;
+		}
+	}
 
-  static getter<T>(_this2: any, property: T | { (): T }): T {
-    if (typeof property === "function") {
-      return (<{ (): T }>property).call(_this2);
-    } else {
-      return <T>property;
-    }
-  }
+	private controller: UIDocumentInteractionController;
 
-  private controller: UIDocumentInteractionController;
+	open(args: any): boolean {
+		if (!args.path) {
+			console.log('ShareFile: Please add a file path');
+			return false;
+		}
 
-  open(args: any): boolean {
-    if (args.path) {
-      try {
-        const appPath = this.getCurrentAppPath();
-        const path = args.path.replace("~", appPath);
+		try {
+			const appPath = this.getCurrentAppPath();
+			const path = args.path.replace('~', appPath);
 
-        this.controller = UIDocumentInteractionController.interactionControllerWithURL(NSURL.fileURLWithPath(path));
-        this.controller.delegate = new UIDocumentInteractionControllerDelegateImpl2();
+			this.controller = UIDocumentInteractionController.interactionControllerWithURL(NSURL.fileURLWithPath(path));
+			this.controller.delegate = new UIDocumentInteractionControllerDelegateImpl2();
 
-        let rect;
-        if (args.rect) {
-          rect = CGRectMake(args.rect.x ? args.rect.x : 0, args.rect.y ? args.rect.y : 0, args.rect.width ? args.rect.width : 0, args.rect.height ? args.rect.height : 0);
-        } else {
-          rect = CGRectMake(0, 0, 0, 0);
-        }
+			let rect;
+			if (args.rect) {
+				rect = CGRectMake(args.rect.x ? args.rect.x : 0, args.rect.y ? args.rect.y : 0, args.rect.width ? args.rect.width : 0, args.rect.height ? args.rect.height : 0);
+			} else {
+				rect = CGRectMake(0, 0, 0, 0);
+			}
 
-        if (args.options) {
-          return this.controller.presentOptionsMenuFromRectInViewAnimated(
-            rect,
-            this.controller.delegate.documentInteractionControllerViewForPreview(this.controller),
-            args.animated ? true : false
-          );
-        } else {
-          return this.controller.presentOpenInMenuFromRectInViewAnimated(
-            rect,
-            this.controller.delegate.documentInteractionControllerViewForPreview(this.controller),
-            args.animated ? true : false
-          );
-        }
+			if (args.options) {
+				return this.controller.presentOptionsMenuFromRectInViewAnimated(rect, this.controller.delegate.documentInteractionControllerViewForPreview(this.controller), args.animated ? true : false);
+			}
 
+			return this.controller.presentOpenInMenuFromRectInViewAnimated(rect, this.controller.delegate.documentInteractionControllerViewForPreview(this.controller), args.animated ? true : false);
+		} catch (e) {
+			console.log('ShareFile: Open file failed', e);
+			return false;
+		}
+	}
 
-      }
-      catch (e) {
-        console.log("ShareFile: Open file failed");
-      }
-    } else {
-      console.log('ShareFile: Please add a file path');
-    }
-    return false;
-  }
+	private getCurrentAppPath(): string {
+		const currentDir = __dirname;
+		const tnsModulesIndex = currentDir.indexOf('/tns_modules');
 
-  private getCurrentAppPath(): string {
-    const currentDir = __dirname;
-    const tnsModulesIndex = currentDir.indexOf("/tns_modules");
-
-    // Module not hosted in ~/tns_modules when bundled. Use current dir.
-    let appPath = currentDir;
-    if (tnsModulesIndex !== -1) {
-      // Strip part after tns_modules to obtain app root
-      appPath = currentDir.substring(0, tnsModulesIndex);
-    }
-    return appPath;
-  }
+		// Module not hosted in ~/tns_modules when bundled. Use current dir.
+		let appPath = currentDir;
+		if (tnsModulesIndex !== -1) {
+			// Strip part after tns_modules to obtain app root
+			appPath = currentDir.substring(0, tnsModulesIndex);
+		}
+		return appPath;
+	}
 }
 
 class UIDocumentInteractionControllerDelegateImpl2 extends NSObject implements UIDocumentInteractionControllerDelegate {
-  public static ObjCProtocols = [UIDocumentInteractionControllerDelegate];
+	public static ObjCProtocols = [UIDocumentInteractionControllerDelegate];
 
-  public getViewController(): UIViewController {
-    const app = ShareFile.getter(UIApplication, UIApplication.sharedApplication);
-    return app.keyWindow.rootViewController;
-  }
+	public getViewController(): UIViewController {
+		const app = ShareFile.getter(UIApplication, UIApplication.sharedApplication);
+		return app.keyWindow.rootViewController;
+	}
 
-  public documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController) {
-    return this.getViewController();
-  }
+	public documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController) {
+		return this.getViewController();
+	}
 
-  public documentInteractionControllerViewForPreview(controller: UIDocumentInteractionController) {
-    return this.getViewController().view;
-  }
+	public documentInteractionControllerViewForPreview(controller: UIDocumentInteractionController) {
+		return this.getViewController().view;
+	}
 }
